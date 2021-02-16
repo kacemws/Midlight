7ebfe5d6d8df7e0049901757982944662ac45a2f
kacemws <berras.belkacem@gmail.com>
2021-02-16 08:48:43 +0100

    adding a section as subcomponent for modal

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,17 +1,28 @@
 import "./Modal.scss";
 
+const Section: React.FC<{}> = (props) => <div className="modal-section"></div>;
 interface Props {
   active: boolean;
   onClick: () => void;
 }
 
-export const Modal: React.FC<Props> = ({ active, onClick, ...rest }) => {
+interface ModalSubComponents {
+  Section: typeof Section;
+}
+
+export const Modal: React.FC<Props> & ModalSubComponents = ({
+  active,
+  onClick,
+  children,
+  ...rest
+}) => {
   return (
     <div
       className={`modal-wrapper ${active ? "active" : ""}`}
       onClick={(_) => onClick()}
     >
-      <div className={`modal ${active ? "active" : ""}`}></div>
+      <div className={`modal ${active ? "active" : ""}`}>{children}</div>
     </div>
   );
 };
+Modal.Section = Section;
