1637fce288be59697c0c0a975bb5cdf458eeab9f
kacemws <berras.belkacem@gmail.com>
2021-02-20 22:50:35 +0100

    init of storybook

---
+++ src/stories/0-Introduction.stories.tsx
@@ -0,0 +1,9 @@
+import { Introduction } from "./Introduction";
+
+// import "../index.css";
+export default {
+  title: "Introduction",
+  component: Introduction,
+};
+
+export const Intro: React.VFC<{}> = () => <Introduction />;
---
+++ src/stories/Introduction.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { PageTitle } from "../Components/Text";
+
+export const Introduction: React.FunctionComponent = function Card() {
+  return (
+    <PageTitle>
+      This is the begining of MidLight ! a whole new design language made by
+      Belkacem Berras to make the process of creating web apps easier!
+    </PageTitle>
+  );
+};
