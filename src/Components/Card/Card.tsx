4acc3ff34102c5d006de5f11a48e467033f45610
kacemws <berras.belkacem@gmail.com>
2021-03-01 14:21:51 +0100

    adding title

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,18 +1,30 @@
+import { Heading } from "../Text";
 import "./Card.scss";
+import { Section } from "./Subcomponents";
 
-interface Props {}
+interface Props {
+  title?: string;
+}
 
-export const Card: React.FC<Props> = ({}) => {
+interface CardSubComponents {
+  Section?: typeof Section;
+}
+
+export const Card: React.FC<Props> & CardSubComponents = ({
+  title,
+  children,
+  ...rest
+}) => {
   return (
     <div className="card">
-      <div className="card-title"></div>
+      <div className="card-title">
+        <Heading>{title}</Heading>
+      </div>
       <div className="card-content">
-        <ol>
-          <li>a</li>
-          <li>abcdef</li>
-        </ol>
+        <ol>{children}</ol>
       </div>
       <div className="card-footer"></div>
     </div>
   );
 };
+Card.Section = Section;
