61a2f7ef992ac24e52d890673d35a4b342c61d51
kacemws <berras.belkacem@gmail.com>
2021-03-02 22:25:54 +0100

    adding stories to the card component

--- src/stories/5-Card.stories.tsx
+++ src/stories/5-Card.stories.tsx
@@ -16,6 +16,113 @@ export const Default: React.VFC<{}> = () => (
       alignItems: "center",
     }}
   >
-    <Card />
+    <div
+      style={{
+        height: "100%",
+        width: "45%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Card>
+        <p>hey</p>
+      </Card>
+    </div>
+  </div>
+);
+
+export const WithTitle: React.VFC<{}> = () => (
+  <div
+    style={{
+      height: "100%",
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div
+      style={{
+        height: "100%",
+        width: "45%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Card title="this is a title">
+        <p>hey</p>
+      </Card>
+    </div>
+  </div>
+);
+
+export const WithPrimaryButton: React.VFC<{}> = () => (
+  <div
+    style={{
+      height: "100%",
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div
+      style={{
+        height: "100%",
+        width: "45%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Card
+        title="this is a title"
+        primaryButton={{
+          title: "this is a primary button",
+          onClick: () => console.log("hey"),
+        }}
+      >
+        <p>hey</p>
+      </Card>
+    </div>
+  </div>
+);
+
+export const WithSection: React.VFC<{}> = () => (
+  <div
+    style={{
+      height: "100%",
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div
+      style={{
+        height: "100%",
+        width: "45%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Card
+        title="this is a title"
+        primaryButton={{
+          title: "this is a primary button",
+          onClick: () => console.log("hey"),
+        }}
+      >
+        <Card.Section title="this is a section title">
+          this is a section
+        </Card.Section>
+        <Card.Section title="this is a section title">
+          this is another section
+        </Card.Section>
+      </Card>
+    </div>
   </div>
 );
